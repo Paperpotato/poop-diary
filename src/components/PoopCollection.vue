6baba0d859c5cfd07e2cabdb068360dd1b4d7707
Paperpotato <cabbage.leaves@gmail.com>
2021-01-10 20:04:59 +0800

    Add donut chart and heatmap
    Add donut chart and heatmap (poopmap lol)

--- src/components/PoopCollection.vue
+++ src/components/PoopCollection.vue
@@ -2,10 +2,24 @@
   <v-container>
     <apexchart
       width="1000"
+      type="heatmap"
+      :options="mapOptions"
+      :series="mapSeries"
+    ></apexchart>
+
+    <apexchart
+      width="1000"
       type="bar"
       :options="chartOptions"
       :series="series"
       ref="demoChart"
+    ></apexchart>
+
+    <apexchart
+      width="1000"
+      type="donut"
+      :options="donutOptions"
+      :series="donutSeries"
     ></apexchart>
 
     <!-- <column-chart :data="rawBankData"></column-chart>
@@ -17,21 +31,59 @@
 <script>
 import { db } from "../firebase";
 import moment from "moment";
-// import apexchart from 'apexcharts'
 
 export default {
-  // components: {
-  //   apexchart
-  // },
   data: () => ({
-    // barData: [
-    //   ["Sun", 32, 10],
-    //   ["Mon", 46, 99],
-    //   ["Tue", 28, 50]
-    // ],
     rawBankData: [],
-    // dataByDate: [],
-    //apexchart data
+    mapOptions: {
+      chart: {
+        height: 350,
+        type: "heatmap"
+      },
+      dataLabels: {
+        enabled: false
+      },
+      colors: ["#008FFB"],
+      title: {
+        text: "PoopMap Chart (Single color)"
+      }
+    },
+    mapSeries: [
+      {
+        name: "Monday",
+        data: [
+          {
+            week: 0,
+            x: "W1",
+            y: 22,
+            z: "1/1/1"
+          },
+          {
+            week: 1,
+            x: "W2",
+            y: 29,
+            z: "1/1/1"
+          }
+        ]
+      },
+      {
+        name: "Tuesday",
+        data: [
+          {
+            week: 0,
+            x: "W1",
+            y: 43,
+            z: "1/1/1"
+          },
+          {
+            week: 2,
+            x: "W2",
+            y: 43,
+            z: "1/1/1"
+          }
+        ]
+      }
+    ],
     chartOptions: {
       chart: {
         id: "time-series"
@@ -40,34 +92,56 @@ export default {
         type: "category"
       }
     },
+    donutOptions: {
+      plotOptions: {
+        pie: {
+          customScale: 0.8,
+          donut: {
+            size: "25%",
+            labels: {
+              show: true,
+              name: {
+                show: true,
+                fontSize: "32px"
+              },
+              value: {
+                show: true,
+                fontSize: "32px"
+              }
+            }
+          }
+        }
+      },
+      chart: {
+        type: "donut"
+      },
+      labels: [
+        "Type 1",
+        "Type 2",
+        "Type 3",
+        "Type 4",
+        "Type 5",
+        "Type 6",
+        "Type 7"
+      ]
+      // total: {
+      //     show: true,
+      //     showAlways: true,
+      //     label: 'Total',
+      //     fontSize: '32px',
+      // }
+    },
     series: [
       {
-        // data: [{x: 'meow', y: 2}]
         data: []
       }
-    ]
+    ],
+    donutSeries: [0, 0, 0, 0, 0, 0, 0]
   }),
-  computed: {
-    // series() {
-    //   return [
-    //     {
-    //       name: "series-1",
-    //       data: [30, 40, 35, 50, 49, 60, 70, 100]
-    //     },
-    //     {
-    //       name: "series-2",
-    //       data: [30, 40, 35, 50, 49, 60, 70, 100]
-    //     }
-    //   ]
-    // }
-  },
+  computed: {},
   methods: {
     test() {
       console.log("meow");
-      // this.series[0].data = {
-      //   x: 'cheese',
-      //   y: 20
-      // }
     }
   },
   created() {
@@ -76,14 +150,70 @@ export default {
       .get()
       .then(querySnapshot => {
         const output = [];
-        const test = [];
+        const pieOutput = [0, 0, 0, 0, 0, 0, 0];
+        const mapOutput = [
+          {
+            name: "Monday",
+            data: []
+          },
+          {
+            name: "Tuesday",
+            data: []
+          },
+          {
+            name: "Wednesday",
+            data: []
+          },
+          {
+            name: "Thursday",
+            data: []
+          },
+          {
+            name: "Friday",
+            data: []
+          },
+          {
+            name: "Saturday",
+            data: []
+          },
+          {
+            name: "Sunday",
+            data: []
+          }
+        ];
         querySnapshot.forEach(doc => {
-          const docData = doc.data();
-          this.rawBankData.push(docData);
+          const poop = doc.data();
+          const poopTimeStamp = moment(poop.timestamp);
+          this.rawBankData.push(poop);
 
-          const poopDate = moment(docData.timestamp).format("DD/MM/YYYY");
-          const poopUnix = moment(docData.timestamp).format("x");
+          const poopDate = poopTimeStamp.format("DD/MM/YYYY");
+          const poopUnix = poopTimeStamp.format("x");
           const foundPoop = output.find(poop => poop.x === poopDate);
+          const poopType = poop.type - 1;
+
+          pieOutput[poopType]++;
+
+          const poopDay = poopTimeStamp.weekday(); //sunday === 0
+          const poopWeek = poopTimeStamp.week(); //week from 1st of Jan
+
+          const foundMapPoop = mapOutput.find(
+            day => day.name === poopTimeStamp.format("dddd")
+          );
+          const foundPoopWeek = foundMapPoop.data.find(
+            week => week.x === `W${poopWeek}`
+          );
+          console.log({ foundPoopWeek });
+          if (!foundPoopWeek) {
+            foundMapPoop.data.push({
+              x: `W${poopWeek}`,
+              y: 1
+            });
+            console.log(foundMapPoop);
+          } else {
+            foundPoopWeek.y++;
+          }
+
+          this.mapSeries = mapOutput;
 
           if (!foundPoop) {
             output.push({
@@ -91,17 +221,13 @@ export default {
               y: 1,
               z: poopUnix
             });
-            test.push(poopUnix);
           } else {
             foundPoop.y++;
-            // poopExists[2] = poopUnix;
           }
         });
 
-        test.sort();
-        console.log(test);
-        // output.sort(function(a, b) { return a.poopUnix > b.poopUnix ? 1 : -1})
-        output.sort((a, b) => a.poopUnix - b.poopUnix);
+        this.donutSeries = pieOutput;
+        output.sort((a, b) => parseInt(a.z) - parseInt(b.z));
         // output.sort()
         console.log("sorted output", output);
         this.series = [
@@ -109,16 +235,11 @@ export default {
             data: output
           }
         ];
-        // this.series[0].data = output;
         this.poopData = output;
-        console.log(this.series);
-        console.log(this.poopData);
       })
       .catch(function(error) {
         console.log("Error getting documents: ", error);
       });
-
-    // console.log(await this.rawBankData)
   }
 };
 </script>
